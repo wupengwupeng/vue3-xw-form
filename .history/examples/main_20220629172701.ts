2aa223e294032eb703b7d4060cd3b3da6ae8b75f
wupeng163 <wupenging163@163.com>
2022-06-29 17:56:51 +0800

    update

--- .history/examples/main_20220629172701.ts
+++ .history/examples/main_20220629172701.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-// import XwForm from '../packages/index'; // 全局挂在form组件
+// import XmwForm from '../packages/index'; // 全局挂在form组件
 // import XwForm from 'vue3-xw-form'
 import XwFrom from 'vue3-xw-form'
 
